c765f463d8af0fab0ef602dfbcac3dfecfd6f2cb
Steven Rosario <strosariogarcia@gmail.com>
2022-09-12 15:47:37 -0400

    ilker: added function to check if more than 3 card already eexists in players deck, if not, append the card

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Deck-Profiler
+
+Deck Profiler allows the user to create a deck based on cards from a myriad of trading card games (e.g. Yugioh, Pokemon, Magic: The Gathering). At the moment, our scope is set to Yugioh for our MVP.
+## API
+
+Ilker and I are utilizing the Yu-Gi-Oh! API by YGOPRODeck (https://ygoprodeck.com/api-guide/) 
+
+## Iteration Methods
+
+.forEach() and .filter() 
+
+## Events
+
+The addEventListener() funcitonality will be implemented into our project. We plan on listening to onclicks, submits, and hopefully hover events.
+
+## Stretch Goals
+
+1) Show all cards from a fuzzy search result
+2) Allow the user to add 3 of the same card into their main deck
+3) Show card's information and price when hovered
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mainDeck = document.getElementById("main_deck");
 const playerDeck = document.getElementById("player_deck");
 const search = document.getElementById("search");
 
+let playerDeckIdArray = [];
+
 const fetchCards = (name) => {
   // Fuzzy search
   return fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=${name}`)
@@ -25,24 +27,38 @@ const createCards = (dataObj) => {
   card.className = "card";
   card.id = characterId;
   imageContainer.src = thumbnailImageUrl;
+  imageContainer.className = "card-image";
   card.append(imageContainer);
 
   // Event Listener to add card to player's Deck
   card.addEventListener("click", () => {
-    playerDeck.append(card);
+    let playerDeckCards = playerDeck.getElementsByClassName("card");
+    
+    // If less than 3 of the same card in player deck, append card.
+    if (isLessThanThree(playerDeckIdArray, card.id)) {
+      playerDeck.append(card.cloneNode(true));
+      playerDeckIdArray.push(card.id);
+    }
   });
-
   mainDeck.append(card);
-
 };
 
-const removeChildNodes = (parent) => {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
+// Function to append card at least 3 times
+const isLessThanThree = (myArray, val) => {
+    return myArray.filter((x) => x === val).length < 3;
+};
+
+const clearList = (parent) => {
+  let first = parent.firstElementChild;
+  while (first) {
+    first.remove();
+    first = parent.firstElementChild;
   }
 };
 
 document.querySelector("#cards-search").addEventListener("submit", (event) => {
   event.preventDefault();
+  clearList(mainDeck);
   fetchCards(event.target.cardname.value);
+  document.querySelector("#cards-search").reset();
 });
