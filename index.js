6e357707c46e970809bee0abd51063e4e35ddf75
Steven Rosario <strosariogarcia@gmail.com>
2022-09-18 22:44:15 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,147 +1,106 @@
-const cardName = document.getElementById("cardname");
-const mainDeck = document.getElementById("main_deck");
-const playerDeck = document.getElementById("player_deck");
-const search = document.getElementById("search");
+const searchBar = document.getElementById("search-bar");
+const searchDeck = document.getElementById("search-deck");
+const playerDeck = document.getElementById("player-deck");
+const cardInfo = document.getElementById("card-info");
 
-let playerDeckIdArray = [];
-
-// ADD FUNCTIONALITY: on click to dropdown menu, select deck and render cards
 let selectDeck = "deck1";
-
-fetch(`http://localhost:3000/${selectDeck}/`)
-  .then((res) => res.json())
-  .then((data) => {
-    data.forEach((card) => {
-      for (let i = 0; i < card.count; i++) {
-        renderPlayerDeck(card);
-        playerDeckIdArray.push(card.id);
-      }
-    });
-    return playerDeckIdArray;
-  })
-  .then((data) => {
-    console.log("initial render playerDeck", data);
-  });
 
 const fetchCards = (name) => {
   // Fuzzy search
   return fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php?fname=${name}`)
     .then((res) => res.json())
     .then((data) => {
-      data.data.forEach((item) => {
-        renderSearchCards(item);
+      // cardData item contains all information on the card!
+      data.data.forEach((cardData) => {
+        renderSearchDeck(cardData);
       });
     });
 };
 
-// Render Functions
-const renderSearchCards = (card) => {
-  const newCard = createCard(card.id, card.card_images[0].image_url_small);
-  // Event Listener to add card to player's Deck
-  newCard.addEventListener("click", () => {
-    addCardToPlayerDeck(newCard);
+// SEARCH DECK RENDER
+const renderSearchDeck = (cardData) => {
+  cardId = cardData.id;
+  cardImageUrl = cardData.card_images[0].image_url_small;
+  const cardHTML = createCardHTML("s", cardId, cardImageUrl);
+  cardHTML.classList.add("search-deck-card");
+  cardHTML.addEventListener("click", () => {
+    addCardToPlayerDeck(cardData);
   });
 
-  mainDeck.append(newCard);
+  renderCardInfo(cardHTML, cardData);
+
+  searchDeck.append(cardHTML);
 };
 
-const renderPlayerDeck = (card) => {
-  let newCard = createCard(card.id, card.image);
+function renderCardInfo(cardHTML, cardData) {
+  cardHTML.querySelector("img").addEventListener("mouseover", () => {
+    const cardInfoHTML = createCardHTML(
+      "c",
+      cardData.id,
+      cardData.card_images[0].image_url
+    );
+    cardInfoHTML.querySelector("img").classList.add("card-info-image");
+    const infoPopup = document.createElement("div");
+    infoPopup.classList.add("card-info-popup");
+    infoPopup.append(cardInfoHTML);
+    const infoContainer = document.createElement("div");
+    infoContainer.classList.add("information-container");
+    const descContainer = document.createElement("div");
+    descContainer.classList.add("description-container");
+    descContainer.textContent = cardData.desc;
 
-  playerDeck.append(newCard);
+    infoContainer.append(descContainer);
+    infoPopup.append(infoContainer);
+    cardInfo.append(infoPopup);
+  });
 
-  addDeleteEvent(newCard, card);
-
-};
-
-
-function addDeleteEvent(newCard, card) {
-  newCard.addEventListener("click", () => {
-    let decrement = card.count - 1;
-    if (card.count > 1) {
-      updatePlayerDeck(selectDeck, card.id, decrement);
-    } else {
-      deletePlayerCard(card.id);
-    }
-    document.getElementById(card.id).remove();
+  cardHTML.querySelector("img").addEventListener("mouseleave", () => {
+    clearList(cardInfo);
   });
 }
 
-// oh it should go to addCardToPlayerDeck no?
-// can we write a function to add delete event? i think so lets try it ok
-//the issue is that dom element we just add are added without delete click events. So where we render dom elements, we need to add delete evenets
-
-function deletePlayerCard(cardId) {
-  fetch(`http://localhost:3000/${selectDeck}/${cardId}`, {
-    method: "DELETE",
-    headers: {
-      "content-type": "application/json",
-    }
-  });
-}
-
-function createCard(id, url) {
+function createCardHTML(searchDeckOrPlayerDeck, id, url) {
   const card = document.createElement("a");
   const imageContainer = document.createElement("img");
 
-  card.className = "card";
-  card.id = id;
+  card.className = `card ${searchDeckOrPlayerDeck}${id}`;
+
   imageContainer.src = url;
   imageContainer.className = "card-image";
   card.append(imageContainer);
   return card;
 }
 
-// Handle Functions
-function addCardToPlayerDeck(card) {
-  if (isLessThanThree(playerDeckIdArray, card.id)) {
-    if (playerDeckIdArray.includes(card.id)) {
-      fetch(`http://localhost:3000/${selectDeck}/${card.id}`)
-        .then((res) => res.json())
-        .then((data) => {
-          let increment = data.count + 1;
-          updatePlayerDeck(selectDeck, card.id, increment);
-        });
-    } else {
-      // note only works if card doesnt exist in deck already
-      postCardToDatabase(card);
-    } 
-    playerDeck.append(card.cloneNode(true))
+// PLAYER DECK RENDER
+function addCardToPlayerDeck(cardData) {
+  const playerCardId = cardData.id;
+  const playercardImageUrl = cardData.card_images[0].image_url_small;
+  let playerCard = createCardHTML("p", playerCardId, playercardImageUrl);
+  let newId = extractIdFromClass(playerCard.className);
+  playerCard.classList.add("player-deck-card");
+  if (
+    document.getElementsByClassName(newId).length < 3 &&
+    document.getElementsByClassName("player-deck-card").length < 60
+  ) {
+    playerCard.addEventListener("click", () => {
+      playerCard.remove();
+      // remove from database
+    });
+    playerDeck.appendChild(playerCard);
+    renderCardInfo(playerCard, cardData);
+    // add to database
+  } else {
+    console.log(`card ${newId} already exists 3 times in deck or deck is full`);
   }
-  playerDeckIdArray.push(card.id);
 }
 
-const updatePlayerDeck = (selectDeck, test, newCount) => {
-  return fetch(`http://localhost:3000/${selectDeck}/${test}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      count: newCount,
-    }),
-  }).then((res) => res.json());
-};
+function extractIdFromClass(classStr) {
+  let str = classStr;
+  let res = str.match(/[sp]\d{7,8}/g);
+  return res[0];
+}
 
-const postCardToDatabase = (card) => {
-  fetch(`http://localhost:3000/${selectDeck}/`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      id: card.id,
-      image: card.querySelector("img").src,
-      count: 1,
-    }),
-  });
-};
-
-// Function to append card at least 3 times
-const isLessThanThree = (myArray, val) => {
-  return myArray.filter((x) => x === val).length < 3;
-};
-
+// SEARCH CALL
 const clearList = (parent) => {
   let first = parent.firstElementChild;
   while (first) {
@@ -150,9 +109,24 @@ const clearList = (parent) => {
   }
 };
 
-document.querySelector("#cards-search").addEventListener("submit", (event) => {
+document.querySelector(".searchbar").addEventListener("submit", (event) => {
   event.preventDefault();
-  clearList(mainDeck);
-  fetchCards(event.target.cardname.value);
-  document.querySelector("#cards-search").reset();
+  clearList(searchDeck);
+  // console.log(event.target.querySelector(".search_input").value);
+  if (event.target.querySelector(".search_input").value.length > 0) {
+    fetchCards(event.target.querySelector(".search_input").value);
+  }
+  document.querySelector(".searchbar").reset();
 });
+
+const yugi = document.getElementById("yugi");
+let counter = 1;
+window.addEventListener("click", () => {
+  if (counter > 3) {
+    counter = 1;
+  }
+  yugi.src = `assets/yugi${counter}.png`;
+  counter = counter + 1;
+});
+
+// add mouseover event to display information on card
